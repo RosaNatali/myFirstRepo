f8da104a08cbc24c71cf2b43a3970f150aa69176
pc <pc@DESKTOP-IEPB48R>
2017-11-10 00:05:13 +0100

    aa
    Signed-off-by: pc <pc@DESKTOP-IEPB48R>

---
+++ test/src/com/esprit/entities/User.java
@@ -0,0 +1,39 @@
+package com.esprit.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer cin;
+	
+	private String name;
+
+	
+	public Integer getCin() {
+		return cin;
+	}
+	
+	
+	public void setCin(Integer cin) {
+		this.cin = cin;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
